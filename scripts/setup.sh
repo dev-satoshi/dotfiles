51d9f89d71fcd81a54a3882dd1ca38c6d0713b49
dev-satoshi <dev.satoshi.1016@gmail.com>
2025-04-21 19:49:52 +0900

    feat: brewのセットアップスクリプトを作成

---
+++ scripts/setup.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env zsh
+set -euo pipefail
+
+echo "「dotfiles」のセットアップを開始しました"
+
+INSTALL_DIR="$HOME/dotfiles"
+
+sh "$INSTALL_DIR/scripts/homebrew_setup.sh"
+
+echo "「dotfiles」のセットアップが完了しました"

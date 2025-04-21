ca8792221a33206fd815c6cdaf8a11eda1cb7f05
dev-satoshi <dev.satoshi.1016@gmail.com>
2025-04-21 18:37:29 +0900

    add: install.sh

---
+++ install.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+set -euo pipefail
+
+INSTALL_DIR="$HOME/dotfiles"
+
+if [ -d "$INSTALL_DIR" ]; then
+  echo "Updating dotfiles..."
+  git -C "$INSTALL_DIR" pull
+else
+  echo "Installing dotfiles..."
+  git clone https://github.com/dev-satoshi/dotfiles "$INSTALL_DIR"
+fi
+
+sh "$INSTALL_DIR/scripts/setup.sh"

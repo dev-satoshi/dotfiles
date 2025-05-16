5712e484c3c58d1a6ecc1e9af69943791fc4c45c
dev-satoshi <dev.satoshi.1016@gmail.com>
2025-05-16 23:46:39 +0900

    fix: スクリプトの登録忘れ

--- scripts/update/update.sh
+++ scripts/update/update.sh
@@ -6,6 +6,7 @@ echo "「dotfiles」のアップデートを開始しました"
 UPDATE_DIR="$HOME/dotfiles"
 
 sh "$UPDATE_DIR/scripts/update/asdf_update.sh"
+sh "$UPDATE_DIR/scripts/update/homebrew_update.sh"
 sh "$UPDATE_DIR/scripts/update/vscode_update.sh"
 
 echo "「dotfiles」のアップデートが完了しました"
